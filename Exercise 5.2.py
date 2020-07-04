df72e4c2932894d2c55fdf2004ff22dfe756d686
soyun20 <anna7a24@gamil.com>
2020-07-04 18:37:58 +0900

    2020/07/24

---
+++ Exercise 5.2.py
@@ -0,0 +1,22 @@
+largest = None
+smallest = None
+num = None
+while num!="done":
+    num = input("Enter a number: ")
+    try:
+        num=int(num)
+        if largest == None:
+            largest=num
+        elif largest<num:
+            largest=num
+
+        if smallest == None:
+            smallest=num
+        elif smallest>num:
+            smallest=num
+    except:
+        if(num!="done"):
+        	print("Invalid input")
+        
+print("Maximum is", largest)
+print("Minimum is", smallest)

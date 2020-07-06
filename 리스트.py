c4c0cc8730e155ff4f9362e8803fdf3b2f972b72
soyun20 <anna7a24@gamil.com>
2020-07-06 18:47:42 +0900

    2020/07/06

---
+++ 리스트.py
@@ -0,0 +1,9 @@
+friends = ['Joseph', 'Glenn', 'Sally']
+carryon = ['socks', 'shirt', 'perfume']
+colors = ['red', ['yellow','blue'], 'black']
+emptyList = []
+print(colors[0])
+lotto = [2, 14, 26, 41, 63]
+print(lotto)
+lotto[2] = 28
+print(lotto)
